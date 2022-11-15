fff3e592c2c7267cde2073b2cb9a5bf5757ba437
rahul01r <rrdubeynibm@gmail.com>
2022-11-15 21:08:50 +0530

    [rahul] Add:UC1-FrequencyOfWord

---
+++ HashTableDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace HashTableDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Hash Table");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            //string hash5 = hash.Get("5");
+            //Console.WriteLine("5th index value:" + hash5);
+            //string hash2 = hash.Get("2");
+            //Console.WriteLine("2nd index value:" + hash2);
+            hash.Remove("5");
+            string hash5 = hash.Get("5");
+
+
+
+
+        }
+    }
+}
